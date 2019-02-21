fd7df4b30c1ffe92cd0130bcfe001c8300bdc5b9
Jay Neupane <jayesh.neupane@gmail.com>
2019-02-20 20:10:28 -0600

    commit

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log("Keys loaded");
+
+exports.spotify = {
+    id: "24bd42e1638d4579aec13f2bd6b94d91",
+    secret: "25f6f405c00d458c9089a0c9757f365b"
+};
+
+exports.twitter = {
+    consumer_key: 'dX4l0PI77TrOMWDgbagVZMwhV',
+    consumer_secret: 'v48oA5AWlkpKiGElxC7sVwqMhmFYqIjIyN8Z2VAdZrrcrX5Z3F',
+    access_token_key: '3128787200-HEH4AFlpF23YbVeJGJIOrhoZUqI7ArJXlsD84Gw',
+    access_token_secret: 'wOEGmL0VHOscdvdQjoVZMwIJFw7q3n1LzKbJDnq25GEEP'
+};
