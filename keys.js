837b702f2707b7d9847145711cd433d6309e4d20
Jay Neupane <jayesh.neupane@gmail.com>
2019-03-06 16:19:47 -0600

    commit

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log("Keys loaded");
 
-exports.spotify = {
+/*exports.spotify = {
     id: "24bd42e1638d4579aec13f2bd6b94d91",
     secret: "25f6f405c00d458c9089a0c9757f365b"
 };
@@ -10,4 +10,25 @@ exports.twitter = {
     consumer_secret: 'v48oA5AWlkpKiGElxC7sVwqMhmFYqIjIyN8Z2VAdZrrcrX5Z3F',
     access_token_key: '3128787200-HEH4AFlpF23YbVeJGJIOrhoZUqI7ArJXlsD84Gw',
     access_token_secret: 'wOEGmL0VHOscdvdQjoVZMwIJFw7q3n1LzKbJDnq25GEEP'
-};
+};*/
+
+
+var twitterKeys = {
+    consumer_key: 'dX4l0PI77TrOMWDgbagVZMwhV',
+    consumer_secret: 'v48oA5AWlkpKiGElxC7sVwqMhmFYqIjIyN8Z2VAdZrrcrX5Z3F',
+    access_token_key: '3128787200-HEH4AFlpF23YbVeJGJIOrhoZUqI7ArJXlsD84Gw',
+    access_token_secret: 'wOEGmL0VHOscdvdQjoVZMwIJFw7q3n1LzKbJDnq25GEEP'
+  };
+  
+  var spotifyKeys = {
+    id: "24bd42e1638d4579aec13f2bd6b94d91",
+    secret: "25f6f405c00d458c9089a0c9757f365b"
+  };
+  
+  var omdbKeys = {
+      id: '40e9cece'
+  }
+  
+  module.exports.twitterKeys = twitterKeys;
+  module.exports.spotifyKeys = spotifyKeys;
+  module.exports.omdbKeys = omdbKeys;
